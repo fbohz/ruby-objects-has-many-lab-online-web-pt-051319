583d58b6cd4bab9325a16d8b5f69adbdc9e1e732
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-06-03 19:39:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,14 @@
 class Song 
   attr_accessor :name, :artist 
+  @@all = []
   
   def initialize(name)
     @name = name 
+    @@all << name
+  end 
+  
+  def self.all
+    @@all
   end 
   
 end 
